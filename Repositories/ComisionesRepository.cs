2053b9fe470ad36e4cb7eee65a157f0446529f9d
Roberto1Rodriguez <robertordzw@gmail.com>
2022-06-01 18:49:31 -0500

    Agregar archivos de proyecto.

---
+++ Repositories/ComisionesRepository.cs
@@ -0,0 +1,20 @@
+﻿using Fruteria_Team.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fruteria_Team.Repositories
+{
+    public class ComisionesRepository
+    {
+        fruteriaContext context = new fruteriaContext();
+
+
+        public IEnumerable<Comisiones> GetAll()
+        {
+            return context.Comisiones.OrderBy(x => x.IdComision);
+        }
+    }
+}
